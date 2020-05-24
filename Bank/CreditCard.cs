841d73a684eb1f656bbe3872b788300d2edbeb34
Tim <690723857@qq.com>
2020-05-25 00:13:58 +0800

    create

--- Bank/CreditCard.cs
+++ Bank/CreditCard.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Bank
 {
-    public partial class Form1 : Form
+    public partial class CreditCard : Form
     {
-        public Form1()
+        public CreditCard()
         {
             InitializeComponent();
         }
