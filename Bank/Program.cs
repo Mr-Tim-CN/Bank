422e83e21e47140c39f72884686c18627e41cf7d
hk000214 <43426115+hk000214@users.noreply.github.com>
2020-05-26 20:41:03 +0800

    Add ForeignExchange's exception's dealing ;Finish TimeDepo's view design

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -16,7 +16,7 @@ namespace Bank
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ForeignExchange());
+            Application.Run(new TimeDeposit());
         }
     }
 }
